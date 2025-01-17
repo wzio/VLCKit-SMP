5c2db23a33eb430f3ad4e8257cfefb008e108508
wangkun <wangkun@variflight.com>
2025-01-17 14:17:58 +0800

    feat: published

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/wzio/VLCKit-SMP/releases/download/3.6.0/VLCKit-all.xcframework.zip", checksum: "7c57222408fb060e8711737747689e1d1fe8273929a82cbc93945d65bf3838b0")
+
+let package = Package(
+    name: "VLCKit-SPM",
+    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    products: [
+        .library(
+            name: "VLCKitSPM",
+            targets: ["VLCKitSPM"]),
+    ],
+    dependencies: [],
+    targets: [
+        vlcBinary,
+        .target(
+            name: "VLCKitSPM",
+            dependencies: [
+                .target(name: "VLCKit-all")
+            ], linkerSettings: [
+                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Security", .when(platforms: [.iOS])),
+                .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
+                .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
+                .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Foundation", .when(platforms: [.macOS])),
+                .linkedLibrary("iconv")
+            ]),
+    ]
+)
